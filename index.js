18fc70d1e7e39e33f70b8380fb42adfb5ff0edff
progfay <shun1856shun@gmail.com>
2020-02-02 15:59:09 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const alfy = require('alfy')
+
+alfy.output(
+  [
+    {
+      title: 'item',
+      subtitle: 'sample of alfred workflow template',
+      arg: '',
+      icon: { path: './icon.png' }
+    }
+  ]
+)
